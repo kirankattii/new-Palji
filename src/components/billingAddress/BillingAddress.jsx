bfe1011f07fe88dcc14848376e8bbe3f45a0ed50
kiran katti <kirankatti550@gmail.com>
2024-08-03 15:52:17 +0530

    added pincode api

--- src/components/billingAddress/BillingAddress.jsx
+++ src/components/billingAddress/BillingAddress.jsx
@@ -180,6 +180,209 @@
 
 // export default BillingAddress;
 
+// import React, { useState } from "react"
+// import { makeApi } from "../../api/callApi"
+// import { ToastContainer, toast } from "react-toastify"
+// import { useNavigate } from "react-router-dom"
+// import axios from "axios"
+
+// import "./billingAddress.css"
+
+// const BillingAddress = () => {
+// 	const navigate = useNavigate()
+
+// 	const [formData, setFormData] = useState({
+// 		name: "",
+// 		phonenumber: "",
+// 		address: "",
+// 		pincode: "",
+// 		country: "",
+// 		state: "",
+// 		city: "",
+// 		GSTNumber: "",
+// 	})
+// 	const [submitting, setSubmitting] = useState(false)
+
+// 	const handleInputChange = (e) => {
+// 		const { name, value } = e.target
+// 		setFormData({
+// 			...formData,
+// 			[name]: value,
+// 		})
+
+// 		if (name === "pincode" && value.length === 6) {
+// 			fetchCityAndState(value)
+// 		}
+// 	}
+
+// 	const fetchCityAndState = async (pincode) => {
+// 		try {
+// 			const response = await axios.get(
+// 				`https://api.example.com/pincode/${pincode}`
+// 			)
+// 			const { city, state } = response.data
+// 			setFormData((prevState) => ({
+// 				...prevState,
+// 				city,
+// 				state,
+// 			}))
+// 		} catch (error) {
+// 			console.error("Error fetching city and state:", error)
+// 			toast.error("Invalid pincode or unable to fetch city and state")
+// 		}
+// 	}
+
+// 	const handleSubmit = async (e) => {
+// 		e.preventDefault()
+// 		if (!formData.name) {
+// 			toast.error("Please fill name")
+// 			return
+// 		}
+
+// 		if (!formData.phonenumber) {
+// 			toast.error("Please fill phonenumber")
+// 			return
+// 		}
+// 		if (!formData.address) {
+// 			toast.error("Please fill address")
+// 			return
+// 		}
+// 		if (!formData.pincode) {
+// 			toast.error("Please fill pincode")
+// 			return
+// 		}
+// 		if (!formData.country) {
+// 			toast.error("Please fill country")
+// 			return
+// 		}
+// 		if (!formData.state) {
+// 			toast.error("Please fill state")
+// 			return
+// 		}
+
+// 		try {
+// 			const response = await makeApi(
+// 				"/api/create-billing-address",
+// 				"POST",
+// 				formData
+// 			)
+// 			console.log("Address created successfully:", response.data)
+// 			if (response.data.success === true) {
+// 				toast.success(response.data.message, {
+// 					onClose: () => {
+// 						navigate("/userprofile/myaddress")
+// 					},
+// 				})
+// 			}
+// 			// Clear form fields after successful submission if needed
+// 			setFormData({
+// 				name: "",
+// 				phonenumber: "",
+// 				address: "",
+// 				pincode: "",
+// 				country: "",
+// 				state: "",
+// 				city: "",
+// 				GSTNumber: "",
+// 			})
+// 		} catch (error) {
+// 			console.error("Error creating address:", error)
+// 			toast.error(error.response.data.message)
+// 		}
+// 	}
+
+// 	return (
+// 		<>
+// 			<ToastContainer autoClose={1000} />
+// 			<div className="my-shipping-belling-address">
+// 				<form
+// 					action=""
+// 					className="address-form"
+// 					onSubmit={handleSubmit}
+// 				>
+// 					<div className="my-billing-address">
+// 						<h2>Billing Address</h2>
+// 						{/* <div className="same-add-checkbox">
+// 							<input type="checkbox" />
+// 							<p>Use same as shipping address</p>
+// 						</div> */}
+// 						<input
+// 							type="text"
+// 							name="name"
+// 							placeholder="Name"
+// 							value={formData.name}
+// 							onChange={handleInputChange}
+// 						/>
+// 						<input
+// 							type="text"
+// 							name="phonenumber"
+// 							placeholder="Phone Number"
+// 							value={formData.phonenumber}
+// 							onChange={handleInputChange}
+// 						/>
+// 						<textarea
+// 							name="address"
+// 							cols="30"
+// 							rows="5"
+// 							placeholder="Address"
+// 							value={formData.address}
+// 							onChange={handleInputChange}
+// 						></textarea>
+// 						<div className="add-pin-country">
+// 							<input
+// 								type="text"
+// 								name="pincode"
+// 								placeholder="Pincode"
+// 								value={formData.pincode}
+// 								onChange={handleInputChange}
+// 							/>
+// 							<input
+// 								type="text"
+// 								name="country"
+// 								placeholder="Country"
+// 								value={formData.country}
+// 								onChange={handleInputChange}
+// 							/>
+// 						</div>
+// 						<div className="add-state-city">
+// 							<input
+// 								type="text"
+// 								name="state"
+// 								placeholder="State"
+// 								value={formData.state}
+// 								onChange={handleInputChange}
+// 							/>
+// 							<input
+// 								type="text"
+// 								name="city"
+// 								placeholder="City"
+// 								value={formData.city}
+// 								onChange={handleInputChange}
+// 							/>
+// 						</div>
+// 						<input
+// 							type="text"
+// 							name="GSTNumber"
+// 							placeholder="GST Number"
+// 							value={formData.GSTNumber}
+// 							onChange={handleInputChange}
+// 						/>
+// 						<button
+// 							className="edit-address-btn"
+// 							type="submit"
+// 							disabled={submitting}
+// 						>
+// 							{submitting ? "Submitting..." : "Save"}
+// 						</button>
+// 					</div>
+// 				</form>
+// 			</div>
+// 		</>
+// 	)
+// }
+
+// export default BillingAddress
+
 import React, { useState } from "react"
 import { makeApi } from "../../api/callApi"
 import { ToastContainer, toast } from "react-toastify"
@@ -211,24 +414,27 @@ const BillingAddress = () => {
 		})
 
 		if (name === "pincode" && value.length === 6) {
-			fetchCityAndState(value)
+			fetchCityStateCountry(value)
 		}
 	}
 
-	const fetchCityAndState = async (pincode) => {
+	const fetchCityStateCountry = async (pincode) => {
 		try {
 			const response = await axios.get(
-				`https://api.example.com/pincode/${pincode}`
+				`https://api.postalpincode.in/pincode/${pincode}`
 			)
-			const { city, state } = response.data
+			const postOfficeData = response.data[0].PostOffice[0]
+			const { State, Country, District: city } = postOfficeData
+
 			setFormData((prevState) => ({
 				...prevState,
 				city,
-				state,
+				state: State,
+				country: Country,
 			}))
 		} catch (error) {
-			console.error("Error fetching city and state:", error)
-			toast.error("Invalid pincode or unable to fetch city and state")
+			console.error("Error fetching city, state, and country:", error)
+			toast.error("Invalid pincode or unable to fetch city, state, and country")
 		}
 	}
 
@@ -261,6 +467,7 @@ const BillingAddress = () => {
 		}
 
 		try {
+			setSubmitting(true)
 			const response = await makeApi(
 				"/api/create-billing-address",
 				"POST",
@@ -288,6 +495,8 @@ const BillingAddress = () => {
 		} catch (error) {
 			console.error("Error creating address:", error)
 			toast.error(error.response.data.message)
+		} finally {
+			setSubmitting(false)
 		}
 	}
 
@@ -302,10 +511,6 @@ const BillingAddress = () => {
 				>
 					<div className="my-billing-address">
 						<h2>Billing Address</h2>
-						{/* <div className="same-add-checkbox">
-							<input type="checkbox" />
-							<p>Use same as shipping address</p>
-						</div> */}
 						<input
 							type="text"
 							name="name"
